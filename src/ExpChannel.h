d9a857f6ba8c76a60421b48ea41e52684932240f
Michael Meier <mmeier1986@googlemail.com>
2016-09-29 14:09:19 +0200

    Add exponential distributed channel model
    This model draws the SINR coefficients from an exponential distribution.

---
+++ src/ExpChannel.h
@@ -0,0 +1,27 @@
+/**
+ * @file ExpChannel.h
+ * @class ExpChannel ExpChannel.h
+ * @brief This channel draws coefficients from an exponential distribution.
+ */
+
+#include "Channel.h"
+#include "includes.h"
+#include "Position.h"
+#include <vector>
+
+class ExpChannel: public Channel{
+	private:
+		double expMean;
+
+		void recomputeCoefficients(
+				const std::vector<std::vector<Position>>& msPositions);
+	
+	public:
+		bool init(cSimpleModule* module,
+				const std::vector<std::vector<Position>>& msPositions, 
+				std::map<int,Position>& neighbourPositions);
+
+		void updateChannel(const std::vector<std::vector<Position>>& msPos);
+
+		virtual ~ExpChannel(){}
+};
